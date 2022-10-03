500767d64ed2e92b318b5c0f70522c25eef2e1cc
gnendraaw <itsgnendraaw@gmail.com>
2022-10-03 17:37:02 +0800

    add getter setter for order system

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -8,10 +8,10 @@
             <div class="row">
                 @foreach($products as $product)
                 <div class="col-3 g-3">
-                    <button class="card text-start w-100 prodBtn" data-id={{$product->id}} data-index="{{$loop->index}}">
+                    <button class="card text-start w-100 prodBtn" data-index="{{$loop->index}}">
                         <div class="card-body">
                             <p class="h5 fw-bold">{{$product->name}}</p>
-                            <p>Stock: {{$product->stock}}</p>
+                            <p>Stock: <span id="prodStock">{{$product->stock}}</span></p>
                             <p class="h5">Rp {{$product->price}}</p>
                         </div>
                     </button>
@@ -28,7 +28,7 @@
                         <p>{{$item->qty}}</p>
                         <div class="row">
                             <button class="btn btn-primary minBtn">-</button>
-                            <input type="number" class="form-control prodQty" id="" min="0">
+                            <input type="number" class="form-control prodQty" id="modalProdStock" min="0">
                             <button class="btn btn-primary plusBtn">+</button>
                         </div>
                     </div>
@@ -44,7 +44,7 @@
 <script src="{{asset('js/jquery.js')}}"></script>
 <script>
     $(document).ready(function() {
-        const products = <?php echo json_encode($products) ?>;
+        var products = <?php echo json_encode($products) ?>;
         var orderItems = {};
 
         $.ajaxSetup({
@@ -53,25 +53,56 @@
             }
         });
 
-        $('.prodBtn').click(function() {
-            const id = $(this).data('id');
-            const index = $(this).data('index');
+        addOrderItem();
 
-            if(orderItems[id])
-                orderItems[id]['qty']++;
-            else
-            {
-                // add order item to array
-                orderItems[id] = {
-                    'qty' : 1,
-                };
+        function addOrderItem()
+        {
+            $('.prodBtn').click(function() {
+                const index = $(this).data('index');
+                const id = products[index]['id'];
 
-                var cartItem = '<div class="card m-3"><div class="card-body"><p>'+products[index]['name']+'</p><div class="row"><button class="btn btn-primary minBtn">-</button><input type="number" class="form-control prodQty" id="" min="1" value="1"><button class="btn btn-primary plusBtn">+</button></div></div></div>';
+                if(products[index]['stock'] > 0)
+                {
+                    orderItems[id] ? orderItems[id]['qty']++ : appendItem(index, id);
 
-                $('#cartList').append(cartItem);
-            }
+                    updateProdStock(index, 1);
+                    $(this).find('#prodStock').html(getProdStock(index));
 
-            console.log('items', orderItems)
+                    console.log('items', orderItems)
+                    console.log('product', products[index])
+                }
+            });
+        }
+
+        function updateProdStock(index, value, isPlus = false)
+        {
+            products[index]['stock'] += isPlus ? value : -value;
+        }
+
+        function setProdStock(index, value)
+        {
+            products[index]['stock'] = value;
+        }
+
+        function getProdStock(index)
+        {
+            return products[index]['stock'];
+        }
+
+        function appendItem(index, id)
+        {
+            orderItems[id] = {
+                'product' : products[index]['name'],
+                'qty' : 1,
+            };
+
+            var cartItem = '<div class="card m-3"><div class="card-body"><p>'+products[index]['name']+'</p><div class="row"><button class="btn btn-primary minBtn">-</button><input type="number" class="form-control prodQty" id="" min="1" value="1"><button class="btn btn-primary plusBtn">+</button></div></div></div>';
+
+            $('#cartList').append(cartItem);
+        }
+
+        $('.plusBtn').click(function() {
+
         });
 
         // $('.prodBtn').click(function() {
