9b16ba1a4950b46200f318c14c0e78fc5a6f0958
gnendraaw <itsgnendraaw@gmail.com>
2022-10-04 16:43:50 +0800

    update order system(uncomplete)

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -7,7 +7,7 @@
             <div class="row">
                 @foreach($products as $product)
                 <div class="col-3 g-3">
-                    <button class="card text-start w-100 prodBtn" data-index="{{$loop->index}}">
+                    <button class="card text-start w-100 prodBtn" data-item="{{$product->id}}">
                         <div class="card-body">
                             <p class="h5 fw-bold">{{$product->name}}</p>
                             <p>Stock: <span id="prodStock">{{$product->stock}}</span></p>
@@ -20,7 +20,7 @@
         </div>
         <div class="col-4">
             <div class="container" id="cartList">
-                @foreach($cart as $item)
+                {{-- @foreach($cart as $item)
                 <div class="card m-3 cartItem" data-id="{{$item->product_id}}">
                     <div class="card-body">
                         <p>{{$item->product->name}}</p>
@@ -31,7 +31,7 @@
                         </div>
                     </div>
                 </div>
-                @endforeach
+                @endforeach --}}
             </div>
         </div>
     </div>
@@ -42,6 +42,7 @@
 <script>
     $(document).ready(function() {
         var products = <?php echo json_encode($products) ?>;
+        var productList = {};
         var orderItems = {};
 
         $.ajaxSetup({
@@ -50,79 +51,98 @@
             }
         });
 
-        addOrderItem();
+        setProductList();
+        addCartItem();
         plusBtn();
 
-        function addOrderItem()
+        function setProductList()
         {
-            $('.prodBtn').click(function() {
-                const index = $(this).data('index');
-                const id = products[index]['id'];
+            for(let i = 0; i < products.length; ++i)
+            {
+                const prod = products[i];
+                const id = prod['id'];
+                const name = prod['name'];
+                const stock = prod['stock'];
 
-                if(products[index]['stock'] > 0)
-                {
-                    orderItems[id] ? orderItems[id]['qty']++ : appendItem(index, id);
-
-                    updateProdStock(index, 1);
-                    $(this).find('#prodStock').html(getProdStock(index));
-
-                    console.log('items', orderItems)
-                    console.log('product', products[index])
+                productList[id] = {
+                    'name' : name,
+                    'stock' : stock
                 }
-            });
+            }
+
+            console.log(productList);
         }
 
         function plusBtn()
         {
-            $('.plusBtn').click(function() {
-                const cartItem = $(this).parents('.cartItem');
-                const id = cartItem.data('id');
-
+            $('#cartList').on('click', '.plusBtn', function() {
+                const id = $(this).parents('.cartItem').data('cartItem');
                 const product = products.find(function(product, index) {
                     if(product.id == id) return true;
                 });
-
-                console.log('id', id);
+                
+                console.log('product', product);
             });
         }
 
-        function updateProdStock(index, value, isPlus = false)
+        function addCartItem()
         {
-            products[index]['stock'] += isPlus ? value : -value;
+            $('.prodBtn').click(function() {
+                const id = $(this).data('item');
+
+                if(productList[id]['stock'] > 0)
+                {
+                    orderItems[id] ? orderItems[id]['qty']++ : appendItem(id);
+
+                    updateProdStock(id, -1);
+
+                    console.log('items', orderItems)
+                    console.log('product', productList[id])
+                }
+            });
         }
 
-        function setProdStock(index, value)
+        function updateCartQty(id)
         {
-            products[index]['stock'] = value;
+
         }
 
-        function getProdStock(index)
+        function updateProdStock(id, val)
         {
-            return products[index]['stock'];
+            productList[id]['stock'] += val;
+            $('[data-item="'+id+'"]').find('#prodStock').html(getProdStock(id));
         }
 
-        function appendItem(index, id)
+        function setProdStock(id, value)
+        {
+            productList[id]['stock'] = value;
+        }
+
+        function getProdStock(id)
+        {
+            return productList[id]['stock'];
+        }
+
+        function appendItem(id)
         {
             orderItems[id] = {
-                'product' : products[index]['name'],
+                'product' : productList[id]['name'],
                 'qty' : 1,
             };
 
             var cartItem = `
-                <div class="card m-3 cartItem" data-id="` + id + `">
+                <div class="card m-3 cartItem" data-cartItem="` + id + `">
                     <div class="card-body">
-                        <p>`+ products[index]['name'] +`</p>
+                        <p>`+ productList[id]['name'] +`</p>
                         <div class="row">
                             <button class="btn btn-primary minBtn">-</button>
-                            <input type="number"  class="form-control cartItemQty" value="1" min="1" max="`+ products[index]['stock'] +`">
+                            <input type="number"  class="form-control cartItemQty" value="1" min="1" max="`+ productList[id]['stock'] +`">
                             <button class="btn btn-primary plusBtn">+</button>
                         </div>
                     </div>
                 </div>`;
 
             $('#cartList').append(cartItem);
-
-            plusBtn();
         }
 
         // $('.prodBtn').click(function() {
