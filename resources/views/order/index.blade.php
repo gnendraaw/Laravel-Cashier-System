bc81e5a4caf65953089c137e223f56a4af7c8a86
gnendraaw <itsgnendraaw@gmail.com>
2022-10-04 19:49:48 +0800

    sync product stock with order qty

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -20,18 +20,6 @@
         </div>
         <div class="col-4">
             <div class="container" id="cartList">
-                {{-- @foreach($cart as $item)
-                <div class="card m-3 cartItem" data-id="{{$item->product_id}}">
-                    <div class="card-body">
-                        <p>{{$item->product->name}}</p>
-                        <div class="row">
-                            <button class="btn btn-primary minBtn">-</button>
-                            <input type="number"  class="form-control cartItemQty" value="1" min="1" max="{{$item->product->stock}}">
-                            <button class="btn btn-primary plusBtn">+</button>
-                        </div>
-                    </div>
-                </div>
-                @endforeach --}}
             </div>
         </div>
     </div>
@@ -54,6 +42,7 @@
         setProductList();
         addCartItem();
         plusBtn();
+        minBtn();
 
         function setProductList()
         {
@@ -63,26 +52,16 @@
                 const id = prod['id'];
                 const name = prod['name'];
                 const stock = prod['stock'];
+                const price = prod['price'];
 
                 productList[id] = {
                     'name' : name,
-                    'stock' : stock
+                    'stock' : stock,
+                    'price' : price,
                 }
             }
 
             console.log(productList);
-        }
-
-        function plusBtn()
-        {
-            $('#cartList').on('click', '.plusBtn', function() {
-                const id = $(this).parents('.cartItem').data('cartItem');
-                const product = products.find(function(product, index) {
-                    if(product.id == id) return true;
-                });
-                
-                console.log('product', product);
-            });
         }
 
         function addCartItem()
@@ -92,9 +71,7 @@
 
                 if(productList[id]['stock'] > 0)
                 {
-                    orderItems[id] ? orderItems[id]['qty']++ : appendItem(id);
-
-                    updateProdStock(id, -1);
+                    orderItems[id] ? plusItemQty(id) : appendItem(id);
 
                     console.log('items', orderItems)
                     console.log('product', productList[id])
@@ -102,15 +79,56 @@
             });
         }
 
-        function updateCartQty(id)
+        function minBtn()
         {
+            $('#cartList').on('click', '.minBtn', function() {
+                const id = $(this).parents('.cartItem').data('cartitem');
 
+                minusItemQty(id);
+            });
+        }
+
+        function plusBtn()
+        {
+            $('#cartList').on('click', '.plusBtn', function() {
+                const cartItem = $(this).parents('.cartItem');
+                const id = cartItem.data('cartitem');
+
+                plusItemQty(id);
+            });
         }
 
         function updateProdStock(id, val)
         {
             productList[id]['stock'] += val;
+
             $('[data-item="'+id+'"]').find('#prodStock').html(getProdStock(id));
+            $('[data-cartitem="'+id+'"]').find('.cartItemQty').val(orderItems[id]['qty']);
+        }
+
+        function minusItemQty(id)
+        {
+            if(orderItems[id]['qty'] >= 1)
+                orderItems[id]['qty']--;
+
+            if(orderItems[id]['qty'] == 0)
+                removeCartItem(id);
+
+            updateProdStock(id, 1);
+        }
+
+        function removeCartItem(id)
+        {
+            $('[data-cartitem="'+id+'"]').remove();
+        }
+
+        function plusItemQty(id)
+        {
+            if(productList[id]['stock'] > 0)
+            {
+                orderItems[id]['qty']++;
+                updateProdStock(id, -1);
+            }
         }
 
         function setProdStock(id, value)
@@ -130,8 +148,10 @@
                 'qty' : 1,
             };
 
+            updateProdStock(id, -1);
+
             var cartItem = `
-                <div class="card m-3 cartItem" data-cartItem="` + id + `">
+                <div class="card m-3 cartItem" data-cartitem="`+id+`">
                     <div class="card-body">
                         <p>`+ productList[id]['name'] +`</p>
                         <div class="row">
